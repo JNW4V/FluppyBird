7fb794717eaf1ec4927657aac482e4057300c418
JNW4V <169117461+JNW4V@users.noreply.github.com>
2024-07-28 13:16:11 -0500

    Update fluppyBirdMovement.cpp

--- fluppyBirdMovement.cpp
+++ fluppyBirdMovement.cpp
@@ -10,24 +10,13 @@
 #define height 3
 
 using namespace std;
-void esperarTeclaOTiempo(int timeout_ms);
 void OcultarCursor();
 void gotoxy(int x, int y);
 void showImage(char image[][width], int pos[][width][2]);
-void newChangePos(int positions[][width][2], char op);
 void changePos(int positions[][width][2], char op);
 void mantenerJuego(char image[][width], int positions[][width][2]);
-void choosePosition(char& op);
+void newChoosePosition(char& op);
 
-/*
-int main() {
-    int timeout = 5000; // 5000 milisegundos = 5 segundos
-    std::cout << "Esperando tecla o tiempo..." << std::endl;
-    esperarTeclaOTiempo(timeout);
-    std::cout << "Programa continuado" << std::endl;
-    return 0;
-}
-*/
 int main()
 {
     char pajaro[height][width] = { {'-','-','-','-','-',' '},
@@ -80,54 +69,9 @@ void changePos(int positions[][width][2], char op) {
     }
 }
 
-void newChangePos(int positions[][width][2], char op) {
-    int pos, k;
-    cout << "Hola";
-    switch (op) {
-    case 'a':
-        pos = 0;
-        k = -1;
-        break;
-    case 'd':
-        pos = 0;
-        k = 1;
-        break;
-    case 'w':
-        pos = 2;
-        k = -1;
-        break;
-    case 's':
-        pos = 1;
-        k = 1;
-        break;
-    }
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            positions[i][j][pos] += k;
-        }
-    }
-}
-/*
-void choosePosition(char &op) {
-
-    gotoxy(40, 40);
-    do {
-
-        cout << "\nOPCIONES: " << endl;
-        cout << "a) Izquierda " << endl;
-        cout << "d) Derecha " << endl;
-        cout << "w) Arriba " << endl;
-        cout << "s) Abajo " << endl;
-        cout << "q) Salir " << endl;
-        cout << "Escoja su opcion: ";
-        op = _getch();
-
-    } while (op != 'a' && op != 'd' && op != 'w' && op != 's' && op != 'q');
-}
-*/
-void newchoosePosition(char& op) {
+void newChoosePosition(char& op) {
     auto start = std::chrono::high_resolution_clock::now();
-    int timeout_ms = 250;  //Este es el tiempo de espera para que el jugador presione una tecla
+    int timeout_ms = 250;  //Este es el tiempo de espera para que el jugador presione una tecla, esta en milisegundos
     gotoxy(40, 40);
         cout << "\nOPCIONES: " << endl;
         cout << "a) Izquierda " << endl;
@@ -150,7 +94,7 @@ void newchoosePosition(char& op) {
             auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count();
             if (duration >= timeout_ms) {
                 std::cout << "Tiempo agotado!" << std::endl;
-                op = 'z';  //Cualquiervalor para que no pase nada
+                op = 'z';  //Cualquier valor para que exista caida
                 return;
             }
             std::this_thread::sleep_for(std::chrono::milliseconds(10));
@@ -184,28 +128,3 @@ void OcultarCursor() {
 }
 
 
-void esperarTeclaOTiempo(int timeout_ms) {
-    auto start = std::chrono::high_resolution_clock::now();
-
-    while (true) {
-        // Si se ha presionado una tecla
-        if (_kbhit()) {
-            // Lee la tecla para vaciar el buffer
-            _getch();
-            std::cout << "Tecla presionada!" << std::endl;
-            return;
-        }
-
-        // Comprueba si ha pasado el tiempo de espera
-        auto now = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count();
-
-        if (duration >= timeout_ms) {
-            std::cout << "Tiempo agotado!" << std::endl;
-            return;
-        }
-
-        // Espera un corto periodo para evitar consumir demasiado CPU
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
-}
